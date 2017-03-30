eea387be7d18211304274efc2b386f889ec49ab3
sw325 <sw325@duke.edu>
2017-03-29 21:49:42 -0400

    lesson2

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -19,12 +19,23 @@ from email_preprocess import preprocess
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
-
-
-
 #########################################################
 ### your code goes here ###
-
+import numpy as np
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
+#features_train = features_train[:len(features_train)/100]
+#labels_train = labels_train[:len(labels_train)/100]  
+#cc = [10,100,1000,10000]
+#for i in cc:
+svc = SVC(C = 10000, kernel = 'rbf')
+t0 = time()
+svc.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+pred = svc.predict(features_test)
+#acc = accuracy_score(labels_test, pred)
+num = np.count_nonzero(pred)
+print num
 #########################################################
 
 
