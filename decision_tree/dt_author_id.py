01c480a1d292ec03fb2f6dd0befd08c738dad8f9
sw325 <sw325@duke.edu>
2017-03-30 14:33:22 -0400

    lesson3

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -19,11 +19,17 @@ from email_preprocess import preprocess
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
-
-
-
 #########################################################
 ### your code goes here ###
+from sklearn import tree
+from sklearn.metrics import accuracy_score
+clf = tree.DecisionTreeClassifier(min_samples_split=40)
+clf.fit(features_train, labels_train)
+labels_pred = clf.predict(features_test)
+acc = accuracy_score(labels_test,labels_pred)
+num_features = len(features_train[0])
+print acc
+
 
 
 #########################################################
